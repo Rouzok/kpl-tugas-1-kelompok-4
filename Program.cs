246c4c063b4c566660700e3125d0f988012aabd2
Miranisa3113 <miratunn91@gmail.com>
2026-03-29 21:09:44 +0700

    state based_Mira

--- Program.cs
+++ Program.cs
@@ -1,82 +1,131 @@
-﻿using QuizInggris_TableDriven;
-using System;
+﻿using System;
 
-namespace QuizInggris_TableDriven
+class GameState
 {
-    public enum DaftarKata
+    enum State
     {
-        thelf, wainch, going, anslip, anspe, see, an, good,
-        caffie, sier, elm, nineteen, water, brother, easy, give, @new, daiy
+        MAIN_MENU,
+
+        MODE1_MENU, MODE1_LEVEL1, MODE1_LEVEL2, MODE1_LEVEL3,
+        MODE2_MENU, MODE2_LEVEL1, MODE2_LEVEL2, MODE2_LEVEL3,
+        MODE3_MENU, MODE3_LEVEL1, MODE3_LEVEL2, MODE3_LEVEL3
     }
 
-    class PemeriksaKata
-    {
-        private bool[] tabelValidasi = {
-                false, // thelf
-                false, // wainch
-                true,  // going
-                false, // anslip
-                false, // anspe
-                true,  // see
-                true,  // an
-                true,  // good
-                false, // caffie
-                false, // sier
-                true,  // elm
-                true,  // nineteen
-                true,  // water
-                true,  // brother
-                true,  // easy
-                true,  // give
-                true,  // new
-                false  // daiy
-            };
+    static State currentState = State.MAIN_MENU;
 
-        public bool CekKebenaran(DaftarKata kata)
+    static void Main(string[] args)
+    {
+        while (true)
         {
-            return tabelValidasi[(int)kata];
+            Console.WriteLine("\nSekarang di: " + currentState);
+            ShowMenu();
+
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+
+            if (currentState == State.MAIN_MENU && input == "0")
+                break;
+
+            HandleInput(input);
         }
     }
 
-
-    // Kemungkinan Implementasi di windows app
-    public class QuizForm
+    static void ShowMenu()
     {
-        private PemeriksaKata logic = new PemeriksaKata();
-
-        // Simulasi Event Handler saat tombol "Submit" diklik di UI
-        public void btnSubmit_Click(List<string> pilihanUser)
+        switch (currentState)
         {
-            Console.WriteLine("--- Hasil Pengecekan Jawaban ---");
-            int kataYangBenar = 11;
-            int skor = 0;
+            case State.MAIN_MENU:
+                Console.WriteLine("1. Mode 1");
+                Console.WriteLine("2. Mode 2");
+                Console.WriteLine("3. Mode 3");
+                Console.WriteLine("0. Exit");
+                break;
 
-            foreach (string kataInput in pilihanUser)
-            {
-                if (Enum.TryParse(kataInput, out DaftarKata kataEnum))
-                {
-                    if (logic.CekKebenaran(kataEnum))
-                    {
-                        Console.WriteLine($"[BENAR] {kataInput} adalah kata bahasa Inggris nyata.");
-                        skor++;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"[SALAH] {kataInput} bukan kata bahasa Inggris nyata.");
-                    }
-                }
-            }
-            Console.WriteLine($"\nTotal Skor: {skor} dari {kataYangBenar} kata dipilih.");
+            case State.MODE1_MENU:
+            case State.MODE2_MENU:
+            case State.MODE3_MENU:
+                Console.WriteLine("1. Start");
+                Console.WriteLine("0. Main Menu");
+                break;
+
+            case State.MODE1_LEVEL3:
+            case State.MODE2_LEVEL3:
+            case State.MODE3_LEVEL3:
+                Console.WriteLine("0. Main Menu");
+                break;
+
+            default:
+                Console.WriteLine("1. Next");
+                Console.WriteLine("0. Main Menu");
+                break;
         }
     }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            QuizForm simulation = new QuizForm();
-            List<string> kataDipilih = new List<string> { "going", "thelf", "water", "new" };
 
-            simulation.btnSubmit_Click(kataDipilih);
+    static void HandleInput(string input)
+    {
+        switch (currentState)
+        {
+            case State.MAIN_MENU:
+                if (input == "1") currentState = State.MODE1_MENU;
+                else if (input == "2") currentState = State.MODE2_MENU;
+                else if (input == "3") currentState = State.MODE3_MENU;
+                break;
+
+            // MODE MENU
+            case State.MODE1_MENU:
+                if (input == "1") currentState = State.MODE1_LEVEL1;
+                else if (input == "0") currentState = State.MAIN_MENU;
+                break;
+
+            case State.MODE2_MENU:
+                if (input == "1") currentState = State.MODE2_LEVEL1;
+                else if (input == "0") currentState = State.MAIN_MENU;
+                break;
+
+            case State.MODE3_MENU:
+                if (input == "1") currentState = State.MODE3_LEVEL1;
+                else if (input == "0") currentState = State.MAIN_MENU;
+                break;
+
+            // MODE 1
+            case State.MODE1_LEVEL1:
+                currentState = NextOrMenu(input, State.MODE1_LEVEL2);
+                break;
+            case State.MODE1_LEVEL2:
+                currentState = NextOrMenu(input, State.MODE1_LEVEL3);
+                break;
+            case State.MODE1_LEVEL3:
+                if (input == "0") currentState = State.MAIN_MENU;
+                break;
+
+            // MODE 2
+            case State.MODE2_LEVEL1:
+                currentState = NextOrMenu(input, State.MODE2_LEVEL2);
+                break;
+            case State.MODE2_LEVEL2:
+                currentState = NextOrMenu(input, State.MODE2_LEVEL3);
+                break;
+            case State.MODE2_LEVEL3:
+                if (input == "0") currentState = State.MAIN_MENU;
+                break;
+
+            // MODE 3
+            case State.MODE3_LEVEL1:
+                currentState = NextOrMenu(input, State.MODE3_LEVEL2);
+                break;
+            case State.MODE3_LEVEL2:
+                currentState = NextOrMenu(input, State.MODE3_LEVEL3);
+                break;
+            case State.MODE3_LEVEL3:
+                if (input == "0") currentState = State.MAIN_MENU;
+                break;
         }
+    }
+
+    static State NextOrMenu(string input, State next)
+    {
+        if (input == "1") return next;
+        if (input == "0") return State.MAIN_MENU;
+        return currentState;
     }
 }
